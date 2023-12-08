e7c827dcd88429334e07d6949dae6320b461dca4
unknown <21bad024>
2023-12-08 11:09:08 +0530

    sample

---
+++ sample.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the num1:"))
+b=int(input("Enter the num2:"))
+c=int(input("Enter the num3:"))
+if a>b && a>c:
+	print("a is greater")
+if b>a && b>c:
+	print("b is greater")
+else:
+	print("c is greater")
